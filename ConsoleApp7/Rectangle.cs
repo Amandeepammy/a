64a700866694248207ef4df1cc36b4146ae6422c
Amandeep Singh <ammydyora@gmail.com>
2019-05-25 19:27:18 -0400

    Change Commited

--- ConsoleApp7/Rectangle.cs
+++ ConsoleApp7/Rectangle.cs
@@ -1,4 +1,4 @@
-﻿ using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp7
 {
-    class Program
+    class Rectangle
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
